2a80c36570d94989f056b07e21f3bfa41191fdb6
Charles <charles.lamb.dev@gmail.com>
2023-04-15 22:01:08 -0400

    removed unnecessary files and changed name of folder to correct file.... lets get started

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/SquintV2Application.java
@@ -0,0 +1,13 @@
+package com.example.SquintV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquintV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquintV2Application.class, args);
+	}
+
+}
