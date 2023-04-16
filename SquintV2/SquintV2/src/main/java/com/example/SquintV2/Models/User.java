7cc13c6ebe200994cede0dc949f33166f5fdb07c
Charles <charles.lamb.dev@gmail.com>
2023-04-16 00:34:21 -0400

    added proper imports for my model class

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/User.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/User.java
@@ -1,6 +1,10 @@
 package main.java.com.example.SquintV2.Models;
 
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
