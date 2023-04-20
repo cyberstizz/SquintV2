e59a6b248d7b8323c17c2e57167e5d30d03f6977
Charles <charles.lamb.dev@gmail.com>
2023-04-20 12:27:52 -0400

    extended the crudrepository with the user model

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/UserRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/UserRepository.java
@@ -2,3 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import main.java.com.example.SquintV2.Models.Users;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    
+}
