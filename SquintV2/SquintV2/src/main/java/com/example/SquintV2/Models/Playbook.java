1005bce71ce6a0193728165575c3d0bbcf3dc642
Charles <charles.lamb.dev@gmail.com>
2023-04-21 10:53:30 -0400

    added the starter code for the playbook table

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
@@ -5,3 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="playbook")
+public class Playbook {
+    
+}
