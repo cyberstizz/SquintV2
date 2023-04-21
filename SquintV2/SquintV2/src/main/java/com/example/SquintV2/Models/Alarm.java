d0685d96436f6cc2073097efa2843649203a3131
Charles <charles.lamb.dev@gmail.com>
2023-04-21 15:29:16 -0400

    added the imports for the alarm model file

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Alarm.java
@@ -0,0 +1,16 @@
+package main.java.com.example.SquintV2.Models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import main.java.com.example.SquintV2.Enums.Alarms;
+import main.java.com.example.SquintV2.Enums.Check_completion;
+import main.java.com.example.SquintV2.Enums.Conflicts;
+import main.java.com.example.SquintV2.Enums.Reminder;
+import main.java.com.example.SquintV2.Enums.Reschedules;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
