b5910c7e429b89a9467b7b49ea194442267b478e
Charles <charles.lamb.dev@gmail.com>
2023-04-21 10:52:06 -0400

    completed import statements

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
@@ -1,3 +1,7 @@
 package main.java.com.example.SquintV2.Models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
