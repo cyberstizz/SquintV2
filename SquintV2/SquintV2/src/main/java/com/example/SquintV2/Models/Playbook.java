9541f4bbc19000ad45d6743fc9c9e16fe36ea0f1
Charles <charles.lamb.dev@gmail.com>
2023-04-21 11:55:26 -0400

    added check completion column that will draw from the check completion enum

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Playbook.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import main.java.com.example.SquintV2.Enums.Check_completion;
 import main.java.com.example.SquintV2.Enums.Reminder;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
@@ -25,5 +26,8 @@ public class Playbook {
 
     @Column(name= "reminder_time")
     private Date reminder_time;
+
+    @Column(name= "check_completion")
+    private Check_completion check_completion;
 
 }
