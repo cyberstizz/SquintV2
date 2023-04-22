00a47a4b2d1363ea25d74bf151c1083d65dec324
Charles <charles.lamb.dev@gmail.com>
2023-04-22 19:02:42 -0400

    got rid unused imports

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Alarm.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Alarm.java
@@ -7,11 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import main.java.com.example.SquintV2.Enums.Alarms;
-import main.java.com.example.SquintV2.Enums.Check_completion;
-import main.java.com.example.SquintV2.Enums.Conflicts;
-import main.java.com.example.SquintV2.Enums.Reminder;
-import main.java.com.example.SquintV2.Enums.Reschedules;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 
