fb0dbd31b6fb23e4338fc6871c9a7b15877d4fb3
Charles <charles.lamb.dev@gmail.com>
2023-04-23 01:38:01 -0400

    added import statments for mailbox table

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Mailbox.java
@@ -0,0 +1,11 @@
+package main.java.com.example.SquintV2.Models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
