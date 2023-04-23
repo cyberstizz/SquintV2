80755dde89f2d23cc1460dbbda738e51be691e13
Charles <charles.lamb.dev@gmail.com>
2023-04-23 01:20:49 -0400

    added primary key for quote table

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Quote.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Quote.java
@@ -7,5 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name="quote")
+public class Quote {
+
+    @GeneratedValue
+    @Id
+    private Integer quote_id;
+
+}
