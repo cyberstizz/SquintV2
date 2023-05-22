16b8b99b80709865c0edf9a69261aef9e199a183
Charles <charles.lamb.dev@gmail.com>
2023-05-21 23:20:13 -0400

    created the setup code for the cassandra repo but i did not add the type yet

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/PlaybookRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/PlaybookRepository.java
@@ -2,3 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
+public interface PlaybookRepository extends CassandraRepository<>{
+    
+}
+
