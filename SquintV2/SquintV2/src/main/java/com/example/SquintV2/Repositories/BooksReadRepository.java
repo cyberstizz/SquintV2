80adc957d0e073e873e4a2d743c77eb2a654e64f
Charles <charles.lamb.dev@gmail.com>
2023-05-22 19:57:45 -0400

    fixed error in booksreadrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/BooksReadRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/BooksReadRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface BooksRepository extends CassandraRepository<Playbook, UUID>{
+public interface BooksReadRepository extends CassandraRepository<Playbook, UUID>{
 
 }
 
