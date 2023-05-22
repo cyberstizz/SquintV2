a9a6e6d4153f2ffd2048171f092ea3088984d30e
Charles <charles.lamb.dev@gmail.com>
2023-05-21 23:21:07 -0400

    added the type and index type to the cassandra repo code

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/PlaybookRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/PlaybookRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface PlaybookRepository extends CassandraRepository<>{
-    
+public interface PlaybookRepository extends CassandraRepository<Playbook, UUID>{
+
 }
 
