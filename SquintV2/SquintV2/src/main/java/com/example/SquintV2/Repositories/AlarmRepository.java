40ff9132aa6048c3e321a77b8e66cdb7467ae30a
Charles <charles.lamb.dev@gmail.com>
2023-05-22 20:20:04 -0400

    fixed code for alarm repository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AlarmRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AlarmRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface AlarmRepository extends CassandraRepository<Playbook, UUID>{
+public interface AlarmRepository extends CassandraRepository<Alarm, UUID>{
 
 }
 
