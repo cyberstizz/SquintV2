dedc0dcb1787a0969d4c8209e4e3a7b169aead30
Charles <charles.lamb.dev@gmail.com>
2023-05-22 20:21:07 -0400

    fixed code for daily shedulerepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/DailyScheduleRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/DailyScheduleRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface DailyScheduleRepository extends CassandraRepository<Playbook, UUID>{
+public interface DailyScheduleRepository extends CassandraRepository<DailySchedule, UUID>{
 
 }
 
