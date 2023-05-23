fd5e12a1aebbc44705be907a353adf358c96c976
Charles <charles.lamb.dev@gmail.com>
2023-05-23 13:06:39 -0400

    fixed the imports for th ebooksread table

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/BooksRead.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/BooksRead.java
@@ -1,13 +1,16 @@
 package main.java.com.example.SquintV2.Models;
 
 import java.time.LocalDate;
+
+import jakarta.persistence.Id;
+
+
 import java.util.UUID;
 
-import org.w3c.dom.Text;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 
 
 @Table(name="books_read")
