498a0a021cba309562ac32700524f7590d2bbe70
Charles <charles.lamb.dev@gmail.com>
2023-05-22 20:22:35 -0400

    fixed code for booksrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/BooksRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/BooksRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface BooksRepository extends CassandraRepository<Playbook, UUID>{
+public interface BooksRepository extends CassandraRepository<Books, UUID>{
 
 }
 
