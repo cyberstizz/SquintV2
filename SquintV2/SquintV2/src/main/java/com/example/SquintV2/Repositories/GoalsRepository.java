239bc1a015fe07990f9f614fac1673efdef48da2
Charles <charles.lamb.dev@gmail.com>
2023-05-22 20:22:01 -0400

    fixed code for goalsrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/GoalsRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/GoalsRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface GoalsRepository extends CassandraRepository<Playbook, UUID>{
+public interface GoalsRepository extends CassandraRepository<Goals, UUID>{
 
 }
 
