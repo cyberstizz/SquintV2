1e5a462d04d7b03dc77df6ede48422addb514fed
Charles <charles.lamb.dev@gmail.com>
2023-05-22 20:11:24 -0400

    fixed error in mailbox repository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/MailboxRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/MailboxRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface MaiilboxRepository extends CassandraRepository<Playbook, UUID>{
+public interface MaiilboxRepository extends CassandraRepository<Mailbox, UUID>{
 
 }
 
