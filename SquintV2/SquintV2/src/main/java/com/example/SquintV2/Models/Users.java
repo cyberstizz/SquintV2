d8a0432dd94f38e8c2ae2a53ef6b9cf6487bb1be
Charles <charles.lamb.dev@gmail.com>
2023-05-24 17:30:29 -0400

    added all columns and getters and setters for the users table

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Users.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Users.java
@@ -15,50 +15,94 @@ import org.springframework.data.cassandra.core.mapping.Table;
 public class Users {
 
     @Id
+    @PrimaryKeyColumn(name = "user_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     private Integer user_id;
 
-    @Column(name="username")
+    @Column(value = "icon_path_or_url")
+    private String icon_path_or_url;
+
+    @Column(value = "username")
     private String username;
 
-    @Column(name="password_hash")
+    @Column(value = "password_hash")
     private String password_hash;
 
-    @Column(name="email")
+    @Column(value = "email")
     private String email;
 
-    @Column(name="accesible")
+    @Column(value = "biography")
+    private String biography;
+
+    @Column(value = "birthday")
+    private LocalDate birthday;
+
+    @Column(value = "accesible")
     private Boolean accessible;
 
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getIcon_path_or_url() {
+        return icon_path_or_url;
+    }
+
+    public void setIcon_path_or_url(String icon_path_or_url) {
+        this.icon_path_or_url = icon_path_or_url;
+    }
 
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPasswordHash() {
-        return this.password_hash;
+    public String getPassword_hash() {
+        return password_hash;
     }
 
-    public void setPasswordHash(String password_hash) {
+    public void setPassword_hash(String password_hash) {
         this.password_hash = password_hash;
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
     public Boolean getAccessible() {
-        return this.accessible;
+        return accessible;
     }
 
     public void setAccessible(Boolean accessible) {
         this.accessible = accessible;
     }
+
+
+    
 }
