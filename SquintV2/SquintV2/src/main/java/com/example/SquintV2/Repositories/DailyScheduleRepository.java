3ce6cb982bfbf119d1f1e03fe01234945da05486
Charles <charles.lamb.dev@gmail.com>
2023-05-24 17:34:00 -0400

    fixed the dailyschedule repository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/DailyScheduleRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/DailyScheduleRepository.java
@@ -1,7 +1,10 @@
 package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface DailyScheduleRepository extends CassandraRepository<DailySchedule, UUID>{
 
 }
