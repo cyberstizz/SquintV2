0ae5f2e7af269279db01ff9f31bcfe4eb070f1ae
Charles <charles.lamb.dev@gmail.com>
2023-05-24 15:47:44 -0400

    fixed all columns and added getters and setters

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Meditation.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Meditation.java
@@ -16,51 +16,61 @@ import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 @Table(name="meditation")
 public class Meditation{
 
-    @Column(name="user_id", nullable=false)
-    private Integer user_id;
+    @Id
+    @PrimaryKeyColumn(name = "user_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private UUID user_id;
 
-    @Column(name="total_sessions")
+    @Column(value = "total_sessions")
     private Integer total_sessions;
     
-    @Column(name="total_time")
+    @Column(value = "total_time")
     private Integer total_time;
 
-    @Column(name="current_streak")
+    @Column(value = "current_streak")
     private Integer current_streak;
 
-    @Column(name="longest_streak")
+    @Column(value = "longest_streak")
     private Integer longest_streak;
 
-    public Integer getUserId() {
-        return this.user_id;
+    public UUID getUser_id() {
+        return user_id;
     }
 
-    public void setUserId(Integer user_id) {
+    public void setUser_id(UUID user_id) {
         this.user_id = user_id;
     }
 
-    public String getTotalTime() {
-        return this.total_time;
+    public Integer getTotal_sessions() {
+        return total_sessions;
     }
 
-    public void setTotalTime(String total_time) {
+    public void setTotal_sessions(Integer total_sessions) {
+        this.total_sessions = total_sessions;
+    }
+
+    public Integer getTotal_time() {
+        return total_time;
+    }
+
+    public void setTotal_time(Integer total_time) {
         this.total_time = total_time;
     }
 
-    public Integer getCurrentStreak() {
-        return this.current_streak;
+    public Integer getCurrent_streak() {
+        return current_streak;
     }
 
-    public void setCurrentStreak(Integer current_streak) {
+    public void setCurrent_streak(Integer current_streak) {
         this.current_streak = current_streak;
     }
 
-    public Integer getLongestStreak() {
-        return this.longest_streak;
+    public Integer getLongest_streak() {
+        return longest_streak;
     }
 
-    public void setLongestStreak(Integer longest_streak) {
+    public void setLongest_streak(Integer longest_streak) {
         this.longest_streak = longest_streak;
     }
+
 
 }
