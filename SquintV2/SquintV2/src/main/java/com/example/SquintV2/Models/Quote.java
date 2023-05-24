3b9b921485682cc0ce65e9b59e66a484cb89ab3a
Charles <charles.lamb.dev@gmail.com>
2023-05-24 17:16:35 -0400

    added new getters and setters for quote

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Quote.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Quote.java
@@ -19,26 +19,36 @@ public class Quote {
     @PrimaryKeyColumn(name = "quote_type", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     private String quote_type;
 
-    @Column(name="quote_author")
+    @Column(value = "quote_author")
     private String quote_author;
 
-    @Column(name="text")
+    @Column(value = "text")
     private String quote;
 
-    public String getQuoteType() {
-        return this.quote_type;
+    public String getQuote_type() {
+        return quote_type;
     }
 
-    public void setQuoteType(String quoteType) {
-        this.quote_type = quoteType;
+    public void setQuote_type(String quote_type) {
+        this.quote_type = quote_type;
+    }
+
+    public String getQuote_author() {
+        return quote_author;
+    }
+
+    public void setQuote_author(String quote_author) {
+        this.quote_author = quote_author;
     }
 
     public String getQuote() {
-        return this.quote;
+        return quote;
     }
 
     public void setQuote(String quote) {
         this.quote = quote;
     }
+
+    
 
 }
