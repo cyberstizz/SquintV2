064a6aca7df66f1089c994818771250fe912a574
Charles <charles.lamb.dev@gmail.com>
2023-05-24 17:36:51 -0400

    fixed the susersrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/UsersRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/UsersRepository.java
@@ -1,7 +1,10 @@
 package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UsersRepository extends CassandraRepository<Users, UUID>{
 
 }
