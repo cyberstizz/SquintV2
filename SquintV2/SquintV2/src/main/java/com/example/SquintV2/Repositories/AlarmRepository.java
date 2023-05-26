021ff11d965befcbd6f680061f195a91bd25e5d1
Charles <charles.lamb.dev@gmail.com>
2023-05-26 00:18:26 -0400

    added uuid import to all repositories

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AlarmRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AlarmRepository.java
@@ -2,6 +2,7 @@ package main.java.com.example.SquintV2.Repositories;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
 
 @Repository
