3814355ea2143ef1a658b193a5a6baa35339602d
Charles <charles.lamb.dev@gmail.com>
2023-05-26 00:28:13 -0400

    added code for playbook service

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/PlaybookService.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/PlaybookService.java
@@ -6,5 +6,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlaybookService {
 
+    private final PlaybookRepository playbookRepository;
+
+    @Autowired
+    public PlaybookService(PlaybookRepository playbookRepository) {
+        this.playbookRepository = playbookRepository;
+    }
+
+    public Playbook getPlaybookSettings(UUID userId) {
+        return playbookRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Playbook settings not found for user: " + userId));
+    }
 
 }
