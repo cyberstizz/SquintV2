f8f8f773de247c5280e642f186801939a6d47c05
Charles <charles.lamb.dev@gmail.com>
2023-05-26 00:26:46 -0400

    added starter code for the playbook service

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/PlaybookService.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/PlaybookService.java
@@ -2,3 +2,9 @@ package main.java.com.example.SquintV2.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+@Service
+public class PlaybookService {
+
+
+}
