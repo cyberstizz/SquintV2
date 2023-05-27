749d13fe3219947ca72288d1fbf768a6998e5611
Charles <charles.lamb.dev@gmail.com>
2023-05-26 22:54:53 -0400

    added all of the imports for the playbook controoller

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/PlaybookController.java
@@ -0,0 +1,10 @@
+package main.java.com.example.SquintV2.Controllers;
+
+import main.java.com.example.SquintV2.Models.Playbook;
+import main.java.com.example.SquintV2.Services.PlaybookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
