2d3cc1740540644b3fb69ea9058403952464ef45
Charles <charles.lamb.dev@gmail.com>
2023-05-29 14:52:14 -0400

    attempted to change dailyschedule item to tasks everywhere in code to be compatible lets see irf it worked

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/TasksRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/TasksRepository.java
@@ -1,13 +1,13 @@
 package main.java.com.example.SquintV2.Repositories;
 
-import main.java.com.example.SquintV2.Models.DailyScheduleItem;
+import main.java.com.example.SquintV2.Models.Tasks;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 
 @Repository
-public interface DailyScheduleRepository extends CassandraRepository<DailyScheduleItem, UUID>{
+public interface TasksRepository extends CassandraRepository<user_id, UUID>{
 
 }
 
