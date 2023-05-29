af37c4aff63fdf58a0631864921ad77ad8ed6856
Charles <charles.lamb.dev@gmail.com>
2023-05-29 14:50:27 -0400

    renamed daily schedule to tasks

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Tasks.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Tasks.java
@@ -17,7 +17,7 @@ import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 
 
 @Table(value="daily_schedule_item")
-public class Task {
+public class Tasks {
 
     @Id
     @PrimaryKeyColumn(name = "user_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
