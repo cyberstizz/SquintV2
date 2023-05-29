4bdc9d1ef643e5dd938d2914a044dab81a22e922
Charles <charles.lamb.dev@gmail.com>
2023-05-29 14:26:41 -0400

    added imports to the goalscontroller file

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/GoalsController.java
@@ -0,0 +1,11 @@
+package main.java.com.example.SquintV2.Controllers;
+
+import main.java.com.example.SquintV2.Models.Goals;
+import main.java.com.example.SquintV2.Services.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
