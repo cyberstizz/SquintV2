1a111423f8998047631c78407b6743c9b19151b0
Charles <charles.lamb.dev@gmail.com>
2023-05-28 23:29:19 -0400

    added imports for the goalsservice

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/GoalsService.java
@@ -0,0 +1,10 @@
+package main.java.com.example.SquintV2.Services;
+
+import main.java.com.example.SquintV2.Models.Goals;
+import main.java.com.example.SquintV2.Repositories.GoalsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
