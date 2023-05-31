970ac9a03f58bd64362d04236f94fd8ef87cbb34
Charles <charles.lamb.dev@gmail.com>
2023-05-31 15:09:36 -0400

    added the imports to the tasks serviceds file

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/TasksService.java
@@ -0,0 +1,9 @@
+package main.java.com.example.SquintV2.Services;
+
+import com.example.SquintV2.Models.Tasks;
+import com.example.SquintV2.Repositories.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
