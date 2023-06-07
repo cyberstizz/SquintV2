95a15d6bb9b0601af9b38189cf9d31ccdad336b0
Charles <charles.lamb.dev@gmail.com>
2023-06-06 23:17:01 -0400

    added call th the taskservice without the type declaration

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/SquintController.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/SquintController.java
@@ -14,6 +14,7 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 
 @Controller
@@ -99,7 +100,7 @@ public class SquintController {
     @GetMapping("dashboard/update-task")
     public ResponseEntity<?> updateATask(UUID tasksId, Tasks updatedTasks){
 
-        TaskService.updateTasks
+        TaskService.updateTasks(tasksId, updatedTasks);
 
     }
 }
