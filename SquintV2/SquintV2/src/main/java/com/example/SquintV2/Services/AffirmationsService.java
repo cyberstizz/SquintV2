29eae935834e39037897347d258b387cc4581031
Charles <charles.lamb.dev@gmail.com>
2023-06-08 22:49:02 -0400

    added else block to the findby id code

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/AffirmationsService.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/AffirmationsService.java
@@ -24,6 +24,7 @@ public class AffirmationsService {
 
     public String getAffirmation(UUID affirmationId) {
 
-        return affirmationRepository.getAffirmationById(affirmationid)
+        return affirmationRepository.findById(affirmationid)
+        .orElseThrow(() -> new RuntimeException("Tasks not found: " + tasksId));
     }
 }
