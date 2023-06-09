0c68aa54fe7d2f764ce1d3b755a1655365467a06
Charles <charles.lamb.dev@gmail.com>
2023-06-08 20:42:53 -0400

    added a field for the affirmation repository ot the affirmationserovice

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/AffirmationsService.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/AffirmationsService.java
@@ -1,6 +1,7 @@
 package main.java.com.example.SquintV2.Services;
 
 import main.java.com.example.SquintV2.Models.Tasks;
+import main.java.com.example.SquintV2.Repositories.AffirmationRepository;
 import main.java.com.example.SquintV2.Repositories.TasksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,6 @@ import java.util.UUID;
 
 @Service
 public class AffirmationsService {
-    
+
+    private final AffirmationRepository affirmationRepository;
 }
