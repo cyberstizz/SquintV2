91cc0ed20bb1b3d6c14544689bf9d73d94355826
Charles <charles.lamb.dev@gmail.com>
2023-06-08 20:41:44 -0400

    added the starter code for the affirmationsservice

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/AffirmationsService.java
@@ -0,0 +1,16 @@
+package main.java.com.example.SquintV2.Services;
+
+import main.java.com.example.SquintV2.Models.Tasks;
+import main.java.com.example.SquintV2.Repositories.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class AffirmationsService {
+    
+}
