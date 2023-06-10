00c5e95ef6edb6877c3a1f909546ff027b5ad60b
Charles <charles.lamb.dev@gmail.com>
2023-06-09 23:21:02 -0400

    autowired in the affirmation service

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/AffirmationController.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/AffirmationController.java
@@ -27,4 +27,9 @@ public class AffirmationController {
     private final AffirmationsService affirmationsService;
 
 
+    @Autowired
+    public AffirmationController(AffirmationsService affirmationsService) {
+        this.affirmationsService = affirmationsService;
+    }
+
 }
