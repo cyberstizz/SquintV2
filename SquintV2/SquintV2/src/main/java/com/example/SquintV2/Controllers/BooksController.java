398736f335459d523a5936f9e2e3a7e12e2350bd
Charles <charles.lamb.dev@gmail.com>
2023-06-11 00:37:20 -0400

    addwed imporst to the bookscontroller

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/BooksController.java
@@ -0,0 +1,10 @@
+import main.java.com.example.SquintV2.Models.Books;
+import main.java.com.example.SquintV2.Services.BooksService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
