7ce2a55319149811c9e69a42c5de99fbe39388f9
Charles <charles.lamb.dev@gmail.com>
2023-06-12 15:32:24 -0400

    updated the imports to include the quote model

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/MeditationService.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/MeditationService.java
@@ -2,17 +2,18 @@ package main.java.com.example.SquintV2.Services;
 
 
 import main.java.com.example.SquintV2.Models.Meditation;
+import main.java.com.example.SquintV2.Models.Quote;
 import main.java.com.example.SquintV2.Repositories.MeditationRepository;
-
+import main.java.com.example.SquintV2.Repositories.QuoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
 @Service
 public class MeditationService {
-    
+
 }
