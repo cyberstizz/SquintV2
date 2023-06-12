054b3a56dd4cd0c7a7eca31df17002da6e798a95
Charles <charles.lamb.dev@gmail.com>
2023-06-12 15:23:27 -0400

    added the imporst for the meditation service

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Services/MeditationService.java
@@ -0,0 +1,15 @@
+package main.java.com.example.SquintV2.Services;
+
+
+import main.java.com.example.SquintV2.Models.Meditation;
+import main.java.com.example.SquintV2.Repositories.MeditationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
