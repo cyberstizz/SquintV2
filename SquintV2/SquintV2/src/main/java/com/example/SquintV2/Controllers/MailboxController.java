5b496c7afc1fc47d051c810537cf8b290c946fea
Charles <charles.lamb.dev@gmail.com>
2023-06-14 01:04:47 -0400

    added all of the methods for the mailboxcontroller

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/MailboxController.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/MailboxController.java
@@ -15,6 +15,38 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/mailbox")
 public class MailboxController {
+    private final MailboxService mailboxService;
+
+    @Autowired
+    public MailboxController(MailboxService mailboxService) {
+        this.mailboxService = mailboxService;
+    }
+
+    @GetMapping("/messages/{recipientId}")
+    public ResponseEntity<List<String>> getMessages(@PathVariable UUID recipientId) {
+        List<String> messages = mailboxService.getMessages(recipientId);
+        return ResponseEntity.ok(messages);
+    }
+
+    @GetMapping("/message/{messageId}")
+    public ResponseEntity<Mailbox> getMessage(@PathVariable UUID messageId) {
+        Optional<Mailbox> message = mailboxService.getMessage(messageId);
+        return message.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Mailbox> sendMessage(@RequestBody Mailbox message) {
+        Mailbox sentMessage = mailboxService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body(sentMessage);
+    }
+
+    @DeleteMapping("/message/{messageId}/user/{userId}")
+    public ResponseEntity<String> deleteMessage(@PathVariable UUID messageId, @PathVariable UUID userId) {
+        mailboxService.deleteMessage(messageId, userId);
+        return ResponseEntity.ok("Message deleted successfully");
+    }
+
     
 }
+
     
