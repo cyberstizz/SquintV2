32ebda89671faae22bd748bc7a38206827e3d00e
Charles <charles.lamb.dev@gmail.com>
2023-06-15 11:57:27 -0400

    changed the productivity repository to the statsrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/ProductivityRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/ProductivityRepository.java
@@ -1,13 +1,13 @@
 package main.java.com.example.SquintV2.Repositories;
 
-import main.java.com.example.SquintV2.Models.Productivity;
+import main.java.com.example.SquintV2.Models.Stats;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 
 @Repository
-public interface ProductivityRepository extends CassandraRepository<Productivity, UUID>{
+public interface StatsRepository extends CassandraRepository<Stats, UUID>{
 
 }
 
