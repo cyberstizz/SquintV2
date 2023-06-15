e79d93a546d05b3fb749fc7ca9eb6e806b477ed4
Charles <charles.lamb.dev@gmail.com>
2023-06-15 17:20:00 -0400

    added the imports for the friendscontroller

---
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/FriendsController.java
@@ -0,0 +1,11 @@
+package main.java.com.example.SquintV2.Controllers;
+
+import main.java.com.example.SquintV2.Models.Friendship;
+import main.java.com.example.SquintV2.Services.FriendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
