0f386f406854b9b41e306d32182e61e324d3d4ca
Charles <charles.lamb.dev@gmail.com>
2023-06-17 21:56:49 -0400

    altered the affirmation model to git rid of the redundant and disruptive @id annotation

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Affirmation.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Models/Affirmation.java
@@ -1,44 +1,33 @@
 package main.java.com.example.SquintV2.Models;
 
-import java.time.LocalDate;
-
-import jakarta.persistence.Id;
-
-
 import java.util.UUID;
-
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 
-
 @Table(value = "affirmation")
 public class Affirmation{
 
-    @Id
     @PrimaryKeyColumn(name = "affirmation_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-    private Integer affirmation_id;
+    private UUID affirmation_id;
 
-    @Id 
     @PrimaryKeyColumn(name = "user_id", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
-    private Integer user_id;
+    private UUID user_id;
 
-    public Integer getAffirmationId() {
+    public UUID getAffirmationId() {
         return this.affirmation_id;
     }
 
-    public void setAffirmationId(Integer affirmation_id) {
+    public void setAffirmationId(UUID affirmation_id) {
         this.affirmation_id = affirmation_id;
     }
 
-
-    public Integer getUserId() {
+    public UUID getUserId() {
         return this.user_id;
     }
 
-    public void setUserId(Integer user_id) {
+    public void setUserId(UUID user_id) {
         this.user_id = user_id;
     }
-
 }
