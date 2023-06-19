dc107de479fecce057266e603f6fe58186bf5d88
Charles <charles.lamb.dev@gmail.com>
2023-06-19 03:02:17 -0400

    deleted references to quojteservice which doesn't exist

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/MeditationController.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/MeditationController.java
@@ -3,7 +3,6 @@ package main.java.com.example.SquintV2.Controllers;
 import main.java.com.example.SquintV2.Models.Meditation;
 import main.java.com.example.SquintV2.Models.Quote;
 import main.java.com.example.SquintV2.Services.MeditationService;
-import main.java.com.example.SquintV2.Services.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +13,10 @@ import java.util.UUID;
 public class MeditationController {
 
     private final MeditationService meditationService;
-    private final QuoteService quoteService;
 
     @Autowired
-    public MeditationController(MeditationService meditationService, QuoteService quoteService) {
+    public MeditationController(MeditationService meditationService) {
         this.meditationService = meditationService;
-        this.quoteService = quoteService;
     }
 
 
