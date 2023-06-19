0f94c063e37b009b3b3bad2e99b2e45781551832
Charles <charles.lamb.dev@gmail.com>
2023-06-19 03:20:24 -0400

    added findbyuserid method to affirmationrepository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AffirmationRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/AffirmationRepository.java
@@ -3,11 +3,13 @@ package main.java.com.example.SquintV2.Repositories;
 import main.java.com.example.SquintV2.Models.Affirmation;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 
 @Repository
 public interface AffirmationRepository extends CassandraRepository<Affirmation, UUID>{
-
+    Optional<Affirmation> findByUserId(UUID userId);
 }
 
