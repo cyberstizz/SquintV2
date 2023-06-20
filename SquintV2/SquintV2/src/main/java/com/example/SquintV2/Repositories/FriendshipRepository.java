d231d7f9c3b95d49cd1196abbb7ebccc0eb69698
Charles <charles.lamb.dev@gmail.com>
2023-06-19 23:07:27 -0400

    added imports to the friendship repository

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/FriendshipRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/FriendshipRepository.java
@@ -3,6 +3,9 @@ package main.java.com.example.SquintV2.Repositories;
 import main.java.com.example.SquintV2.Models.Friendship;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
