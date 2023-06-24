d332e8d8e1557fcec99a5a2915f70f3eb56e7a52
Charles <charles.lamb.dev@gmail.com>
2023-06-24 00:50:43 -0400

    added a method to the tasksrepositroy to fetch tasks withing a specific date range

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/TasksRepository.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Repositories/TasksRepository.java
@@ -4,6 +4,7 @@ import main.java.com.example.SquintV2.Models.Tasks;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,6 +13,8 @@ import java.util.UUID;
 public interface TasksRepository extends CassandraRepository<Tasks, UUID>{
     List<Tasks> getTasksForDay(UUID currentDate);
     Tasks findByUserId(UUID userId);
+    List<Tasks> findByUser_idAndTask_deadlineBetween(UUID user_id, LocalDate start, LocalDate end);
+
 
 }
 
