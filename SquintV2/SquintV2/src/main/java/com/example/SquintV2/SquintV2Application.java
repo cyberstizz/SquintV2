e55c9277033ebcbe8a5c9589f4530ec6ec99a86b
Charles <charles.lamb.dev@gmail.com>
2023-07-13 19:42:07 -0400

    added Cql session builder customizer and more configuration

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/SquintV2Application.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/SquintV2Application.java
@@ -10,4 +10,15 @@ public class SquintV2Application {
 		SpringApplication.run(SquintV2Application.class, args);
 	}
 
+
+	 /**
+     * This is necessary to have the Spring Boot app use the Astra secure bundle 
+     * to connect to the database - a quote from java brains
+     */
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+
 }
