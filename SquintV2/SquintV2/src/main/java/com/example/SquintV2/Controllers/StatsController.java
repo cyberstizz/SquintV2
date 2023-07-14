4230e10cac706b90b687a8ca829fe8efdb1a24dd
Charles <charles.lamb.dev@gmail.com>
2023-07-13 22:04:08 -0400

    changed the name of the zip file in the application.yml

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/StatsController.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Controllers/StatsController.java
@@ -1,3 +1,6 @@
+package com.example.SquintV2.Controllers;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
