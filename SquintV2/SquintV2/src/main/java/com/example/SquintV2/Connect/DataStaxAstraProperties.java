d27971bcdd03f56425d5551d6504cb79f0700c61
Charles <charles.lamb.dev@gmail.com>
2023-07-26 23:15:25 -0400

    changed the post construct to check if the bundle file is readable

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/Connect/DataStaxAstraProperties.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/Connect/DataStaxAstraProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-// import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 
 @Configuration
@@ -18,11 +18,8 @@ public class DataStaxAstraProperties {
 
     
 
-    //  // Add this method for debugging
-    //  @PostConstruct
-    //  public void logProperties() {
-    //      System.out.println("Secure Connect Bundle Path: " + secureConnectBundle);
-    //  }
+     // Add this method for debugging
+     
 
 
     public File getSecureConnectBundle() {
@@ -33,6 +30,22 @@ public class DataStaxAstraProperties {
 
     public void setSecureConnectBundle(File secureConnectBundle) {
         this.secureConnectBundle = secureConnectBundle;
+    }
+
+
+    @PostConstruct
+    public void logProperties() {
+        System.out.println("Secure Connect Bundle Path: " + secureConnectBundle);
+        if (secureConnectBundle != null) {
+            System.out.println("Secure Connect Bundle exists: " + secureConnectBundle.exists());
+            try {
+                System.out.println("Secure Connect Bundle is readable: " + secureConnectBundle.canRead());
+            } catch (SecurityException e) {
+                System.out.println("Unable to check if the Secure Connect Bundle is readable: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Secure Connect Bundle is null.");
+        }
     }
 
     
