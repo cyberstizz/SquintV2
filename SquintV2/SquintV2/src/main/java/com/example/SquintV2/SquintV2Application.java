bd0faed0d0fec463b2387e05a69d36458ba263e9
Charles <charles.lamb.dev@gmail.com>
2023-09-17 21:02:52 -0400

    changed the versions to what I believe is optiamal for this application

--- SquintV2/SquintV2/src/main/java/com/example/SquintV2/SquintV2Application.java
+++ SquintV2/SquintV2/src/main/java/com/example/SquintV2/SquintV2Application.java
@@ -13,6 +13,8 @@ import com.example.SquintV2.connection.DataStaxAstraProperties;
 
 // import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+//https://lucid.app/lucidspark/1d8557dc-94c4-4412-89b2-130e8e8aeb39/edit?invitationId=inv_ad4e5510-e56c-4009-a2a7-a55868bf8200
+
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class SquintV2Application {
@@ -26,7 +28,7 @@ public class SquintV2Application {
      * This is necessary to have the Spring Boot app use the Astra secure bundle 
      * to connect to the database - a quote from java brains
      */
-        @Bean
+    @Bean
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
